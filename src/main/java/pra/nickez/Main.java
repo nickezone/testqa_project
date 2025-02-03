a811eb63f71f058d069809b5bf66d0cdc15a587a
Alexandr Tereshko <nickezpra@gmail.com>
2025-02-03 17:53:08 +0300

    hello messege was changed

--- src/main/java/pra/nickez/Main.java
+++ src/main/java/pra/nickez/Main.java
@@ -2,6 +2,6 @@ package pra.nickez;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, okay.qa!");
     }
 }
